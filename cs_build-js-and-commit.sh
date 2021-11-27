147d089ca5adbb8d60f36778eeaf5539ce76e153
juanitolaguna <juanitolaguna@protonmail.com>
2021-11-27 21:29:42 +0100

    build-11-27-2021-1638044982-6.4.5.0

--- cs_build-js-and-commit.sh
+++ cs_build-js-and-commit.sh
@@ -4,13 +4,14 @@
 
 pluginName=$PLUGINNAME
 
-
+echo "BUILD FOR ${pluginNamem} PLUGIN"
 #docker exec -it shopware /var/www/html/bin/build-administration.sh
 
 #copy new administration
 sudo rm -rf ./src/Resources/public/administration
 docker cp shopware:/var/www/html/custom/plugins/$pluginName/src/Resources/public/. \
 ./src/Resources/public
+
 
 
 #commit build with new timestamp
